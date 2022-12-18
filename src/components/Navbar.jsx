71ac11a2285b5a1a44709c37b778d0fc0ed571a4
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2022-12-19 03:44:24 +0400

    Add: Profile component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,49 @@
-import { AutoAwesomeMotion } from "@mui/icons-material";
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
 import React from "react";
+
+import { AutoAwesomeMotion, Mail, Notifications } from "@mui/icons-material";
+
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Profile from "./Profile";
 
 export default function Navbar() {
   const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
   });
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+  }));
+
+  const Icons = styled(Box)(({ theme }) => ({
+    display: "none",
+    alignItems: "center",
+    gap: "20px",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+  }));
+
+  const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  }));
 
   return (
     <AppBar position="sticky">
@@ -15,6 +52,22 @@ export default function Navbar() {
           Blog
         </Typography>
         <AutoAwesomeMotion sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Profile />
+        </Icons>
+        <UserBox>
+          <Typography variant="span">John</Typography>
+          <Profile />
+        </UserBox>
       </StyledToolbar>
     </AppBar>
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,82 @@
+import { Logout, Settings } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import React from "react";
+
+export default function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <Avatar
+        onClick={handleClick}
+        alt="Remy Sharp"
+        src="./assets/avatars/avatar_9.jpg"
+      />
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+}
