8a7bd7e0db873db8dfe25730f3fa53ab0cbdd47b
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2023-12-04 14:55:00 +0400

    Added file & svg loaders, Changed theme switcher component

---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,32 @@
+import cls from "./Button.module.scss"
+import {classNames} from "shared/lib/classNames/classNames";
+import {ButtonHTMLAttributes, FC} from "react";
+
+export enum ThemeButton {
+  CLEAR = 'clear'
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  className?: string;
+  theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const {
+    onClick,
+    className,
+    children,
+    theme,
+    ...otherProps
+  } = props
+  console.log(props)
+  return (
+    <button
+      onClick={onClick}
+      className={classNames(cls.Button, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  )
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import cls from "./Navbar.module.scss"
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string
@@ -9,14 +10,15 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <AppLink
-        className={cls.mainLink}
-        to={'/'}
-        theme={AppLinkTheme.PRIMARY}
-      >
-        General
-      </AppLink>
+      <ThemeSwitcher />
       <div className={cls.links}>
+        <AppLink
+          className={cls.mainLink}
+          to={'/'}
+          theme={AppLinkTheme.PRIMARY}
+        >
+          General
+        </AppLink>
         <AppLink to={'/about'} theme={AppLinkTheme.INVERTED}>
           About
         </AppLink>
