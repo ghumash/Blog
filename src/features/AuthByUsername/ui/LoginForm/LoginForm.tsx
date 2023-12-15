fb24ef3c5b5ecc4b398b37b73a40eeb02f547fc0
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2023-12-15 20:30:29 +0400

    Add shared input component, Add stories

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTranslation } from 'react-i18next'
+import { Button } from 'shared/ui/Button'
+import { Input } from 'shared/ui/Input'
+import cls from './LoginForm.module.scss'
+
+interface LoginFormProps {
+  className?: string;
+}
+
+export const LoginForm = (props: LoginFormProps) => {
+  const { className } = props
+  const { t } = useTranslation()
+
+  return (
+    <div className={classNames(cls.LoginForm, {}, [className])}>
+      <Input type="text" className={cls.input} />
+      <Input type="text" className={cls.input} />
+      <Button className={cls.loginBtn}>
+        {t('Login')}
+      </Button>
+    </div>
+  )
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import { Modal } from 'shared/ui/Modal'
 import React, { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Button, ButtonTheme } from 'shared/ui/Button'
+import { LoginModal } from 'features/AuthByUsername'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -13,8 +13,12 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation()
   const [isAuthModal, setIsAuthModal] = useState(false)
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev)
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false)
+  }, [])
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true)
   }, [])
 
   return (
@@ -22,15 +26,14 @@ export const Navbar = ({ className }: NavbarProps) => {
       <Button
         theme={ButtonTheme.CLEAR_INVERTED}
         className={cls.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t('Login')}
       </Button>
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at beatae deleniti dicta
-        dolor, ipsam laudantium necessitatibus porro quae sequi.
-      </Modal>
+      <LoginModal
+        isOpen={isAuthModal}
+        onClose={onCloseModal}
+      />
     </div>
   )
 }
